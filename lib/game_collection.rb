3e18342ae3081b595bdad36c7de735b7d03499cb
Linda Le <yueling1222@gmail.com>
2019-10-24 15:38:33 -0600

    Refactor code from stat_tracker into game collection

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -16,4 +16,27 @@ class GameCollection
   def total_games
     @games.length
   end
+
+  def find_count_of_game_by_season
+    games_per_season = Hash.new(0)
+      @games.each { |game| games_per_season[game.season] += 1}
+      games_per_season.sort.to_h
+  end
+
+  def find_average_goals_per_game
+    total_goals = @games.sum { |game| game.total_score }
+      (total_goals.to_f/total_games).round(2)
+  end
+
+  def find_highest_total_score
+    @games.max_by {|game| game.total_score}.total_score
+  end
+
+  def find_lowest_total_score
+    @games.min_by {|game| game.total_score}.total_score
+  end
+
+  def calculate_goal_differences
+    @games.max_by {|game| game.game_goal_difference}.game_goal_difference
+  end
 end
