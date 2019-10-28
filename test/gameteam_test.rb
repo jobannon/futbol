54c462d165af006ef7af843d4d526724de7089a3
Joshua O'bannon <jtobannon@gmail.com>
2019-10-28 15:39:56 -0600

    Finish game_team tests

--- test/gameteam_test.rb
+++ test/gameteam_test.rb
@@ -27,4 +27,22 @@ class GameteamTest < Minitest::Test
   def test_it_exists
     assert_instance_of GameTeam, @gameteam
   end
+
+  def test_it_initializes
+    assert_equal "2012030221", @gameteam.game_id
+    assert_equal 3, @gameteam.team_id
+    assert_equal "away",@gameteam.hoa
+    assert_equal "LOSS",@gameteam.result
+    assert_equal "OT",@gameteam.settled_in
+    assert_equal "John Tortorella", @gameteam.head_coach
+    assert_equal 2, @gameteam.goals
+    assert_equal 8, @gameteam.shots
+    assert_equal 44, @gameteam.tackles
+    assert_equal 8, @gameteam.pim
+    assert_equal 3, @gameteam.power_play_opportunities
+    assert_equal 0, @gameteam.power_play_goals
+    assert_equal 44.8, @gameteam.face_off_win_percentage
+    assert_equal 17, @gameteam.giveaways
+    assert_equal 7, @gameteam.takeaways
+  end
 end
